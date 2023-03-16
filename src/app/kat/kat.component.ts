c154cb187eb0ce5b4dc3421dc89cbcd7e9061b39
unknown <katekani.sibuye@gmail.com>
2023-03-16 15:05:10 +0200

    viewing and liking posts

---
+++ src/app/kat/kat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kat',
+  templateUrl: './kat.component.html',
+  styleUrls: ['./kat.component.css']
+})
+export class KatComponent {
+  
+
+}
